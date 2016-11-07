cfe1588bd6c43eda013b8b59bea76120bc589a3e
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-07 21:09:54 +0100

    konstruktor klasy InOutStreamPump

--- src/main/java/lab3/lib1/InOutStreamPump.java
+++ src/main/java/lab3/lib1/InOutStreamPump.java
@@ -4,9 +4,16 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class InOutStreamPump {
-	
+
 	private byte[] buffer;
 	private InputStream in;
 	private OutputStream out;
+
+	public InOutStreamPump(InputStream in, OutputStream out, int bufSize) {
+		super();
+		this.buffer = new byte[bufSize];
+		this.in = in;
+		this.out = out;
+	}
 
 }
