3b75b396690feb18b32049fb21a7f7bb02796043
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-07 21:06:51 +0100

    metoda HTTP.download

--- src/main/java/lab3/lib1/HTTP.java
+++ src/main/java/lab3/lib1/HTTP.java
@@ -1,12 +1,14 @@
 package lab3.lib1;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 
 public class HTTP {
 
-	public static void download(URL url, OutputStream dest) {
-		
+	public static void download(URL url, OutputStream dest) throws IOException {
+		InputStream src = url.openStream();
 	}
 	
 }
