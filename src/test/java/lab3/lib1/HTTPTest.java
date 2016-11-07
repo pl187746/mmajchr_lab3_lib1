eadeaab810b06779b6b9333c17a52f48f1caf3f2
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-07 21:47:45 +0100

    test pobierajacy plik regulaminu studiow

--- src/test/java/lab3/lib1/HTTPTest.java
+++ src/test/java/lab3/lib1/HTTPTest.java
@@ -1,7 +1,9 @@
 package lab3.lib1;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.net.URL;
 
 import org.junit.Test;
@@ -13,6 +15,15 @@ public class HTTPTest {
 		byte[] content = HTTP.download(new URL("http://google.pl"));
 		String strCnt = new String(content);
 		assertTrue(strCnt.contains("Google"));
+	}
+	
+	@Test
+	public void regulamin() throws Throwable {
+		URL url = new URL("https://www.p.lodz.pl/sites/default/files/pliki/regulamin_studiow_0.pdf");
+		File file = new File("regulamin.pdf");
+		int size = HTTP.download(url, file);
+		assertTrue(file.exists());
+		assertEquals(file.length(), size);
 	}
 
 }
