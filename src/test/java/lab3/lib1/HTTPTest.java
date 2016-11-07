266120bfa3f1e216cd5d90cf8790f88bebd138af
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-07 21:38:08 +0100

    test pobierajacy zawartosc google.pl

--- src/test/java/lab3/lib1/HTTPTest.java
+++ src/test/java/lab3/lib1/HTTPTest.java
@@ -1,12 +1,18 @@
 package lab3.lib1;
 
+import static org.junit.Assert.assertTrue;
+
+import java.net.URL;
+
 import org.junit.Test;
 
 public class HTTPTest {
 
 	@Test
-	public void google() {
-		
+	public void google() throws Throwable {
+		byte[] content = HTTP.download(new URL("http://google.pl"));
+		String strCnt = new String(content);
+		assertTrue(strCnt.contains("Google"));
 	}
 
 }
