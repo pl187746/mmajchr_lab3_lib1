993670f911e2aa613bdc3dae8ed46d34d7352733
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-08 00:02:23 +0100

    test poprawnosci wypakowania

--- src/test/java/lab3/lib1/ZipTest.java
+++ src/test/java/lab3/lib1/ZipTest.java
@@ -1,5 +1,36 @@
 package lab3.lib1;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.junit.After;
+import org.junit.Test;
+
 public class ZipTest {
+	
+	@Test
+	public void unzip() throws IOException {
+		Zip.unzip(new File("test_zip_in.zip"), Paths.get("test_unzipped"));
+		File file = new File("test_unzipped/lol.txt");
+		assertTrue(file.exists());
+	}
+	
+	@After
+	public void deleteUnzipped() {
+		deleteDir(new File("test_unzipped"));
+	}
+	
+	private static void deleteDir(File file) {
+	    File[] contents = file.listFiles();
+	    if (contents != null) {
+	        for (File f : contents) {
+	            deleteDir(f);
+	        }
+	    }
+	    file.delete();
+	}
 
 }
