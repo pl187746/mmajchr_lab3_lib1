846e3b51af372f2c4ead90ca94a3be69ac10d3b6
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-07 23:49:15 +0100

    implementacja metody unzip

--- src/main/java/lab3/lib1/Zip.java
+++ src/main/java/lab3/lib1/Zip.java
@@ -1,12 +1,28 @@
 package lab3.lib1;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class Zip {
 
-	public static void unzip(ZipInputStream zip, File dir) {
-		;
+	public static void unzip(ZipInputStream zip, Path dir) throws IOException {
+		dir.toFile().mkdirs();
+		ZipEntry entry;
+		while((entry = zip.getNextEntry()) != null) {
+			File path = dir.resolve(entry.getName()).toFile();
+			if(entry.isDirectory()) {
+				path.mkdirs();
+			} else {
+				try(FileOutputStream fout = new FileOutputStream(path)) {
+					InOutStreamPump pump = new InOutStreamPump(zip, fout);
+					while(pump.pump() > 0);
+				}
+			}
+		}
 	}
 
 }
