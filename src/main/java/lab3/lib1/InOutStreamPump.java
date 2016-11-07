3d48aa4a16c37c1ac5ff45ab8ee6abe65345ae8c
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-07 21:09:09 +0100

    klasa InOutStreamPump

--- src/main/java/lab3/lib1/InOutStreamPump.java
+++ src/main/java/lab3/lib1/InOutStreamPump.java
@@ -1,5 +1,12 @@
 package lab3.lib1;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class InOutStreamPump {
+	
+	private byte[] buffer;
+	private InputStream in;
+	private OutputStream out;
 
 }
