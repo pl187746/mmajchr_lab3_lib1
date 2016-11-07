348f2457028b334cc60cbd2e5f076346508f3df3
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-08 00:47:29 +0100

    test pakowania jednego pliku

--- src/test/java/lab3/lib1/ZipTest.java
+++ src/test/java/lab3/lib1/ZipTest.java
@@ -3,9 +3,12 @@ package lab3.lib1;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.zip.ZipOutputStream;
 
 import org.junit.After;
 import org.junit.Test;
@@ -34,6 +37,11 @@ public class ZipTest {
 	        }
 	    }
 	    file.delete();
+	}	
+
+	@Test
+	public void zipOne() throws FileNotFoundException, IOException {
+		Zip.zipFile(new File("pom.xml"), new ZipOutputStream(new FileOutputStream(new File("test_zip_out_1.zip"))));
 	}
 
 }
